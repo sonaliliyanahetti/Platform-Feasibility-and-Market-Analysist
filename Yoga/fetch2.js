500128de74efa26fc750dd51e73892465479a11d
sonaliliyanahetti <liyanahettisonali@gmail.com>
2025-05-16 11:34:11 +0530

    web_scrapped data
    Web scrape data using inspect >Network fetch code< get json file from console of modash.io

---
+++ Yoga/fetch2.js
@@ -0,0 +1,32 @@
+let results = [];
+
+for (let page = 1; page < 150; page++) {
+  let response = await fetch("https://marketer.modash.io/api/discovery/search/instagram", {
+    "headers": {
+      "accept": "application/json, text/plain, */*",
+      "accept-language": "en-GB,en;q=0.9",
+      "content-type": "application/json",
+      "priority": "u=1, i",
+      "sec-ch-ua": "\"Google Chrome\";v=\"131\", \"Chromium\";v=\"131\", \"Not_A Brand\";v=\"24\"",
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-platform": "\"macOS\"",
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "same-origin",
+      "x-amzn-trace-id": "Root=1-67e3a3e6-6157bbf0eebcbbf92ef5e278;Parent=213d4061d91b7154;Sampled=1"
+    },
+    "referrer": "https://marketer.modash.io/discovery/instagram",
+    "referrerPolicy": "same-origin",
+    "body": "{\"page\":" +page+ ",\"filters\":{\"influencer\":{\"accountTypes\":[],\"location\":[148838],\"hasContactDetails\":[{\"contactType\":\"email\",\"filterAction\":\"must\"}],\"relevance\":{\"usernames\":[],\"hashtags\":[\"yoga\",\"yogalife\",\"yogalove\",\"yogagirl\",\"yogaeverywhere\"]},\"textTags\":[],\"interests\":[196],\"keywords\":\"\",\"brands\":[],\"followers\":{\"min\":5000,\"max\":50000},\"engagementRate\":0.03},\"audience\":{\"location\":[],\"age\":[],\"interests\":[],\"brands\":[]},\"actions\":[],\"options\":{\"showSavedProfiles\":true}},\"sort\":{}}",
+    "method": "POST",
+    "mode": "cors",
+    "credentials": "include"
+  });
+  let data = (await response.json());
+
+  let people = data.lookalikes;
+  results.push(people);
+}
+
+
+console.log(JSON.stringify(results));
